38d1a5b8c3b9e6d17dbf814c3c867050758d0814
IA <outilsia101@gmail.com>
2025-04-24 02:04:48 +0000

    Chore: Updated CORS headers and verified their presence in chat.ts

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -10,7 +10,7 @@ const openai = new OpenAI({
 });
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://agent10111.github.io');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
